c989ad794c4999cd6e30599dbf328e31b260022c
wangdecheng <1938727123@qq.com>
2024-04-27 09:59:35 +0800

    实现了离线文件和离线消息功能；删除了一条在线状态的无用信息；增加了json消息的格式化输出，可以在配置文件中选择关闭

--- tool/run_client_no_ui.bat
+++ tool/run_client_no_ui.bat
@@ -15,7 +15,7 @@ timeout /t 2 >nul
 rem Start the client
 rem start pythonw /chatapp/client.py
 start cmd /k python ./tool/client_no_ui.py 1
-timeout /t 1 >nul
+timeout /t 2 >nul
 start cmd /k python ./tool/client_no_ui.py 2
 
 rem Wait for the user to press any key before closing the window
